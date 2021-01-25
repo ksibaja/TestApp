145ef568544a0048fee675b0ede2ed017830e115
Kervin Sibaja <rslk0325@gmail.com>
2021-01-24 21:46:14 -0600

    bg color

--- src/components/Topnavbar.js
+++ src/components/Topnavbar.js
@@ -3,23 +3,31 @@ import './styles/Topnavbar.css';
 import { Navbar, Nav } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
-const Topnavbar = () => (
-  <div>
-    <Navbar className="Topnavbar" collapseOnSelect expand="lg" variant="dark">
-      <Navbar.Brand href="/main" style={{ color: 'yellowgreen' }}>Home</Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto" >
-          <LinkContainer to="/vehicles">
-            <Nav.Link >Vehicles</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/manager">
-            <Nav.Link>Manager</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  </div>
-)
+class Topnavbar extends React.Component {
+  componentDidMount() {
+    document.body.style = `background: ${localStorage.getItem('color')};`;
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar className="Topnavbar" collapseOnSelect expand="lg" variant="dark">
+          <Navbar.Brand href="/main" style={{ color: 'yellowgreen' }}>Home</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto" >
+              <LinkContainer to="/vehicles">
+                <Nav.Link >Vehicles</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/manager">
+                <Nav.Link>Manager</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    )
+  }
+}
 
 export default Topnavbar;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,9 +3,22 @@ import Home from "../components/Home";
 
 class Main extends React.Component {
 
+  state = {}
+
   handleClick = () => {
     console.log('Hello, you clicked the button...')
     alert('Hello, you clicked the button...')
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  };
+
+  handleClickColor = () => {
+    localStorage.setItem('color', this.state.color)
+    document.body.style = `background: ${this.state.color};`;
   }
 
   render() {
@@ -24,7 +37,19 @@ class Main extends React.Component {
             <Home />
             <button className="btn btn-secondary" onClick={this.handleClick}>
               Click here o.o
-          </button>
+            </button><hr />
+            <div className="col-md-3" style={{ margin: 'auto' }}>
+              <div className="card">
+                <label>Background color</label>
+                <input
+                  type="text"
+                  name="color"
+                  className="form-control"
+                  onChange={this.handleChange}
+                ></input>
+                <button className="btn btn-primary" onClick={this.handleClickColor}>Save</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
